e824fdf4b22156380b964ab95b3c723061ae6948
saul <43302778+saulwolfcode@users.noreply.github.com>
2019-11-15 14:06:33 -0300

    history

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect, withRouter } from 'react-router-dom'
 
-export default function Navbar() {
+export default function Navbar(props) {
+    const { history } = props;
     return (
-        <header>
-            <h1>TasksApp</h1>
-            <Link to="/">Home</Link> - 
-            <Link to="/About">About</Link> - 
-            <Link to="/Post">Post</Link>
-        </header>
-    )
+      <header>
+        <h1>TasksApp</h1>
+        <Link to="/">Home</Link> -<Link to="/About">About</Link> -
+        <Link to="/Post">Post</Link>
+        <br />
+        <React.Fragment>
+          <button onClick={()=> history.push("/About")}>go to push</button>
+          <button onClick={()=> history.goBack()}>go to back</button>
+          <button onClick={()=> history.goForward()}>go to forward</button>
+        </React.Fragment>
+      </header>
+    );
 }
